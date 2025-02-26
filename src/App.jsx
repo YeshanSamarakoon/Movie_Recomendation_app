0090ae7896a41d98a857050abec5c2b704dc9aa1
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-02-26 20:32:16 +0530

    first com

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+    <div className="pattern">
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find Your Favourite <span className='text-gradient'>Movies</span>
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </div>
+    </main>
+  )
+} 
+
+export default App
