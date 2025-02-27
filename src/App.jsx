1bde6a8b3a66689adf28c60cc905edb60899a576
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-02-28 03:22:37 +0530

    added a 3d card

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Spotlight } from './components/UI/spotlight'
+import Search from './components/Search'
+import ThreeDCardDemo from './components/Card'
 
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
   return (
     <main>
-    <div className="pattern overflow-hidden">
+    <div className="pattern overflow-x-hidden">
       <div className="wrapper">
+        
         <header>
+          
           <h1>
             Find Your Favourite <span className='text-gradient'>Movies</span>
           </h1>
         </header>
-        <p>Search</p>
+        <ThreeDCardDemo/>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
       </div>
       <Spotlight/>
     </div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { CardBody, CardContainer, CardItem } from "./UI/3dCard";
+
+const ThreeDCardDemo = () => {
+  return (
+    <CardContainer className="inter-var">
+      <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-primary  border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
+        <CardItem
+          translateZ="50"
+          className="text-xl font-bold text-neutral-600 dark:text-white text-center justify-center "
+        >
+          Seacrh the best For You
+        </CardItem>
+        <CardItem
+          as="p"
+          translateZ="60"
+          className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+        >
+          
+        </CardItem>
+        <CardItem translateZ="100" className="w-full mt-4">
+          <img
+            src="/src/assets/hero.png"
+            alt="thumbnail"
+            className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+          />
+        </CardItem>
+        
+      </CardBody>
+    </CardContainer>
+  );
+};
+
+export default ThreeDCardDemo;
