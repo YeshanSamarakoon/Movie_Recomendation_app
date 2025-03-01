dab7474398cc9649fd6bc246f0a8b3b8469d43f3
YESHABI <yeshanbi.samarakoon@gmail.com>
2025-03-02 02:07:08 +0530

    Added a tilt option for the movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Spinner from './components/Spinner';
 import MovieCard from './components/MovieCard';
 import { useDebounce } from 'react-use';
 import { getTrendingMovies, updateSearchCount } from './appwrite';
+import { Tilt } from 'react-tilt';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
 
@@ -97,6 +98,17 @@ const App = () => {
       const offset = 100; 
       searchbar.scrollIntoView({top: searchbar.offsetTop - offset, behavior: 'smooth' });
     }
+    const defaultOptions = {
+      reverse:        false,  // reverse the tilt direction
+      max:            35,     // max tilt rotation (degrees)
+      perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+      scale:          1,    // 2 = 200%, 1.5 = 150%, etc..
+      speed:          500,   // Speed of the enter/exit transition
+      transition:     true,   // Set a transition on enter/exit.
+      axis:           null,   // What axis should be disabled. Can be X or Y.
+      reset:          true,    // If the tilt effect has to be reset on exit.
+      easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+    }
 
   return (
     <main>
@@ -142,7 +154,7 @@ const App = () => {
 
               <ul>
                 {movieList.map((movie) => (
-                  <MovieCard key={movie.id} movie={movie} />
+                  <Tilt  options={defaultOptions} style={{ height: 500, width: 250 }}><MovieCard key={movie.id} movie={movie} /></Tilt>
                 ))}
               </ul>
 
